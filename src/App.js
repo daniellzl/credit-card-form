aa2706d75e62b7feb4bceff61d46a0140202cfff
Daniel Liang <mr.daniel.liang@gmail.com>
2021-11-25 14:27:01 -0800

    import ErrorBoundary and CreditCardForm

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import ErrorBoundary from "./components/error-boundary/error-boundary.component.jsx";
+import CreditCardForm from "./components/credit-card-form/credit-card-form.component.jsx";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ErrorBoundary>
+        <CreditCardForm />
+      </ErrorBoundary>
+    </div>
+  );
 }
 
 export default App;
