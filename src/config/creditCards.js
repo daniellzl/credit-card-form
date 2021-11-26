f3a335b975a214f27ce4f55bb2b9c1d164a9c6b8
Daniel Liang <mr.daniel.liang@gmail.com>
2021-11-26 11:27:26 -0800

    add credit format with spaces

--- src/config/creditCards.js
+++ src/config/creditCards.js
@@ -1,12 +1,12 @@
 const creditCards = [
   {
     vendor: "Visa",
-    cardNumberRegex: /^4[0-9]{15}$/,
+    cardNumberRegex: /^4[0-9]{3} [0-9]{4} [0-9]{4} [0-9]{4}$/,
     cvv2Regex: /^[0-9]{3}$/,
   },
   {
     vendor: "American Express",
-    cardNumberRegex: /^3[47][0-9]{13}$/,
+    cardNumberRegex: /^3[47][0-9]{2} [0-9]{6} [0-9]{5}$/,
     cvv2Regex: /^[0-9]{4}$/,
   },
 ];
