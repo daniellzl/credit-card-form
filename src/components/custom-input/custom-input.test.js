a34b5ffd7f597415882faf8e42ff27432664fd67
Daniel Liang <mr.daniel.liang@gmail.com>
2021-11-26 12:16:01 -0800

    don't need this

--- src/components/custom-input/custom-input.test.js
+++ src/components/custom-input/custom-input.test.js
@@ -1,8 +1,6 @@
 import { shallow } from "enzyme";
 import CustomInput from "./custom-input.component.jsx";
 
-console.log(shallow(<CustomInput />).debug());
-
 it("expect to render CustomInput component", () => {
   expect(shallow(<CustomInput />)).toMatchSnapshot();
 });
