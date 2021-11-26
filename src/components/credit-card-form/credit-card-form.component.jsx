007f52e71112041e681540338fad311661bf3455
Daniel Liang <mr.daniel.liang@gmail.com>
2021-11-25 17:08:05 -0800

    use customInput

--- src/components/credit-card-form/credit-card-form.component.jsx
+++ src/components/credit-card-form/credit-card-form.component.jsx
@@ -1,5 +1,7 @@
 import { initialValues, validations } from "./credit-card-form.data.js";
+import CustomInput from "../custom-input/custom-input.component.jsx";
 import useForm from "../../hooks/useForm.js";
+import CSS from "./credit-card-form.module.scss";
 
 const CreditCardForm = () => {
   const [values, errors, handleInput, handleBlur, isValid] = useForm({
@@ -14,75 +16,83 @@ const CreditCardForm = () => {
   };
 
   return (
-    <form>
-      <div>
-        <input
-          id="name"
-          type="text"
-          value={values.name}
-          onInput={handleInput("name")}
-          onBlur={handleBlur("name")}
-        />
-        <label htmlFor="name">Name</label>
-        {errors.name}
-      </div>
-      <div>
-        <input
-          id="card-number"
-          type="text"
-          value={values.cardNumber}
-          onInput={handleInput("cardNumber")}
-          onBlur={handleBlur(["cardNumber", "cardNumberAndCvv2"])}
-        />
-        <label htmlFor="card-number">Card Number</label>
-        {errors.cardNumber}
-      </div>
-      <div>
-        <input
-          id="cvv2"
-          type="text"
-          value={values.cvv2}
-          onInput={handleInput("cvv2")}
-          onBlur={handleBlur(["cvv2", "cardNumberAndCvv2"])}
-        />
-        <label htmlFor="cvv2">CVV2</label>
-        {errors.cvv2}
-      </div>
-      <div>
-        <input
-          id="exp-month"
-          type="text"
-          value={values.expMonth}
-          onInput={handleInput("expMonth")}
-          onBlur={handleBlur(["expMonth", "expMonthAndYear"])}
-          maxLength="2"
-        />
-        <label htmlFor="exp-month">Exp. Month</label>
-        {errors.expMonth}
-      </div>
-      <div>
-        <input
-          id="exp-year"
-          type="text"
-          value={values.expYear}
-          onInput={handleInput("expYear")}
-          onBlur={handleBlur(["expYear", "expMonthAndYear"])}
-          maxLength="4"
-        />
-        <label htmlFor="exp-year">Exp. Year</label>
-        {errors.expYear}
-      </div>
-      <div>
-        <input
-          type="submit"
-          value="Submit"
-          onClick={handleSubmit}
-          disabled={!isValid()}
-        />
-      </div>
-      {errors.cardNumberAndCvv2}
-      {errors.expMonthAndYear}
-    </form>
+    <div className={CSS.creditCardFormFrame}>
+      <form className={CSS.creditCardForm}>
+        <div className={CSS.inputsFrame}>
+          <CustomInput
+            type="text"
+            label="Name"
+            value={values.name}
+            handleInput={handleInput("name")}
+            handleBlur={handleBlur("name")}
+          />
+          <div className={CSS.errorFrame}>{errors.name}</div>
+        </div>
+        <div className={CSS.inputsFrame}>
+          <div className={CSS.cardNumberAndCvv2Frame}>
+            <div className={CSS.cardNumberFrame}>
+              <CustomInput
+                type="text"
+                maxLength="16"
+                label="Card Number"
+                value={values.cardNumber}
+                handleInput={handleInput("cardNumber")}
+                handleBlur={handleBlur(["cardNumber", "cardNumberAndCvv2"])}
+              />
+            </div>
+            <div className={CSS.cvv2Frame}>
+              <CustomInput
+                type="text"
+                maxLength="4"
+                label="CVV2"
+                value={values.cvv2}
+                handleInput={handleInput("cvv2")}
+                handleBlur={handleBlur(["cvv2", "cardNumberAndCvv2"])}
+              />
+            </div>
+          </div>
+          <div className={CSS.errorFrame}>
+            {errors.cardNumber || errors.cvv2 || errors.cardNumberAndCvv2}
+          </div>
+        </div>
+        <div className={CSS.inputsFrame}>
+          <div className={CSS.expMonthAndYearFrame}>
+            <div className={CSS.expMonthFrame}>
+              <CustomInput
+                type="text"
+                maxLength="2"
+                label="Exp. Month"
+                value={values.expMonth}
+                handleInput={handleInput("expMonth")}
+                handleBlur={handleBlur(["expMonth", "expMonthAndYear"])}
+              />
+            </div>
+            <div className={CSS.expYearFrame}>
+              <CustomInput
+                type="text"
+                maxLength="4"
+                label="Exp. Year"
+                value={values.expYear}
+                handleInput={handleInput("expYear")}
+                handleBlur={handleBlur(["expYear", "expMonthAndYear"])}
+              />
+            </div>
+          </div>
+          <div className={CSS.errorFrame}>
+            {errors.expYear || errors.expMonth || errors.expMonthAndYear}
+          </div>
+        </div>
+        <div className={CSS.buttonFrame}>
+          <input
+            type="submit"
+            value="Submit"
+            className={`${CSS.button} ${isValid() ? "" : CSS.disabled}`}
+            onClick={handleSubmit}
+            disabled={!isValid()}
+          />
+        </div>
+      </form>
+    </div>
   );
 };
 
